df4f206f799df06c4909a38e462d812e753da283
Maksim <koramba921@mail.ru>
2025-03-11 22:22:40 +0300

    book homework

---
+++ src/Book.java
@@ -0,0 +1,57 @@
+import java.util.Objects;
+
+public class Book {
+    private String name;
+    private int yearPublication;
+    private Author author;
+
+    public Book() {}
+
+    public Book(Author author, int yearPublication, String name) {
+        this.author = author;
+        this.yearPublication = yearPublication;
+        this.name = name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public int getYearPublication() {
+        return yearPublication;
+    }
+
+    public void setYearPublication(int yearPublication) {
+        this.yearPublication = yearPublication;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return yearPublication == book.yearPublication && Objects.equals(name, book.name) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, yearPublication, author);
+    }
+
+    @Override
+    public String toString() {
+        return "Книга. название: " + name + ", " + author + ", " + yearPublication;
+    }
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Author author1 = new Author("Александр ", "Грибоедов");
+        System.out.println(author1.toString());
+        Author author2 = new Author("Иван ", "Тургенев");
+        System.out.println(author2.toString());
+
+        Book book1 = new Book(author1, 1822, "Горе от ума" );
+        Book book2 = new Book(author2, 1860, "Отцы и дети");
+
+        System.out.println(book1.toString());
+        System.out.println(book2.toString());
+
+        book1.setYearPublication(1900);
+        System.out.println(book1.toString());
     }
+
 }
